79dfec3e1058ea448ec04b43817d17d36eabf88b
V Anbarasu <anbarasukv@gmail.com>
2017-05-18 15:40:59 +0530

    first

---
+++ src/main/java/com/examplebuk/bukdemo/BukdemoApplication.java
@@ -0,0 +1,12 @@
+package com.examplebuk.bukdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukdemoApplication.class, args);
+	}
+}
